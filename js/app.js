9e32cc908e22eabc0818e98fef531d612644af15
Maximinsavi <toessijeannette69@gmail.com>
2026-06-15 17:02:16 +0100

    js/app.js erstellen

---
+++ js/app.js
@@ -0,0 +1,3 @@
+Parse.initialize("BHxYtdxdSPMSH5vXHVdKCVTBrxMBlbPLhz2p4CRc", "88Fp770fFyWoBEXCljw9QhlaY6EPYcx9kR1sVpZV");
+Parse.serverURL = "https://parseapi.back4app.com";
+Parse.enableLocalDatastore();
